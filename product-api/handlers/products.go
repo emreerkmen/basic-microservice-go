2b848c9c36bf557ccba4f6bd1afed1dfe9b2ee7c
Emre Erkmen <emre.erkmen@hotmail.com>
2022-03-13 01:08:28 +0300

    Use new encoder instead of marshal

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"basic-microservice/hello/product-api/data"
-	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -11,17 +10,27 @@ type Products struct {
 	l *log.Logger
 }
 
-func NewProducts(l *log.Logger) *Products{
+func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
 func (g *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	listOfProducts := data.GetProducts()
-	json, err := json.Marshal(listOfProducts)
+
+	// That usage is much slower acording to NewEncodeer method that places in product structs.
+	/*
+		json, err := json.Marshal(listOfProducts)
+
+		if err != nil {
+			http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+		}
+
+		rw.Write(json)
+	*/
+
+	err := listOfProducts.ToJSON(rw)
 
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
-
-	rw.Write(json)
 }
