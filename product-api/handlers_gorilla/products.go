e5820fc174ee05a8d1c65d355bc159be7d39bd5e
Emre Erkmen <emre.erkmen@hotmail.com>
2022-05-24 23:52:24 +0300

    Delete unnesecarry code

--- product-api/handlers_gorilla/products.go
+++ product-api/handlers_gorilla/products.go
@@ -4,8 +4,6 @@ import (
 	"basic-microservice/hello/product-api/data"
 	"log"
 	"net/http"
-	"regexp"
-	"strconv"
 )
 
 type Products struct {
@@ -16,55 +14,7 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		p.getProducts(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodPost {
-		p.addProduct(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodPut {
-		p.l.Println("PUT", r.URL.Path)
-		// expect the id in the URI
-		reg := regexp.MustCompile(`/([0-9]+)`)
-		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
-
-		if len(g) != 1 {
-			p.l.Println("Invalid URI more than one id")
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		if len(g[0]) != 2 {
-			p.l.Println("Invalid URI more than one capture group")
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		idString := g[0][1]
-		id, err := strconv.Atoi(idString)
-		if err != nil {
-			p.l.Println("Invalid URI unable to convert to numer", idString)
-			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-			return
-		}
-
-		p.l.Println("Id",id)
-		p.updateProducts(id, rw, r)
-		return
-
-	}
-	//catch others
-
-	rw.WriteHeader(http.StatusMethodNotAllowed)
-
-}
-
-func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle GET Products")
 	listOfProducts := data.GetProducts()
 
